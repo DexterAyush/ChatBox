0e6c9b26c8f699b2bcdffb49b69ceb45b1132233
Ayush Srivastava <srivastavaayush1998@gmail.com>
2022-01-27 22:15:57 +0530

    first commit

---
+++ src/main/java/com/springwebsocket/ChatApplicationSpringBootAndWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationSpringBootAndWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationSpringBootAndWebSocketApplication.class, args);
+	}
+
+}
